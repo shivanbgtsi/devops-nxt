175c7e26bce96bbce760c13bfb573f6f93022eb3
shivanbgtsi <shivanagoudabg@gmail.com>
2024-06-01 13:50:50 +0530

    inital commit

---
+++ src/main/java/com/example/demo/DevopsnxtDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsnxtDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsnxtDemoApplication.class, args);
+	}
+
+}
