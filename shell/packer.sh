7fd55b258b124ef378ff5a7be90b553dc05336a6
Shivanagouda Govindagoudar <68856301+shivanbgtsi@users.noreply.github.com>
2024-07-18 18:04:55 +0530

    Update packer.sh

--- shell/packer.sh
+++ shell/packer.sh
@@ -1,21 +1,20 @@
 #!/bin/sh
 
 : '
-  Verify the ami build prerequisites for
-  EPH Semarchy IAC deploy pipelines.
+  Verify the ami build prerequisites
   These specs require https://shellspec.info to run.
 '
 
-Describe "EPH Semarchy prerequisites"
+Describe "Demo prerequisites"
 
   Describe "in AWS parameter store"
-      It "Semarchy value exists - $SEMARCHY_VERSION"
-        When call aws_ssm_by_path "$SEMARCHY_VERSION"
-        The output should include "version"
+      It "Semarchy value exists - $RDS"
+        When call aws_ssm_by_path "$RDS"
+        The output should include "pass"
     End
-      It "Artifactory password exists - $ARTIFACTORY_PASSWORD"
-        When call aws_ssm_by_path "$ARTIFACTORY_PASSWORD"
-        The output should include "artifactory_password"
+      It "Artifactory password exists - $RDS"
+        When call aws_ssm_by_path "$RDS"
+        The output should include "pass"
     End
   End
 End
