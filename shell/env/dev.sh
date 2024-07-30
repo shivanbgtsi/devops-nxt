67a3c43ad9a15406af900749c5bd328f7e3deada
shivanbgtsi <shivanagoudabg@gmail.com>
2024-07-30 18:15:23 +0530

    update

--- shell/env/dev.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export AWS_REGION=ap-south-1
-
-export SEMARCHY_ENVIRONMENT=/dev/rds
-
--- spec/packer.sh
+++ spec/packer.sh
@@ -17,4 +17,14 @@ Describe "Demo prerequisites"
         The output should include "pass"
     End
   End
+  Describe "in AWS parameter store-temp"
+      It "Semarchy value exists - $RDS"
+        When call aws_ssm_by_path "$RDS"
+        The output should include "pass"
+    End
+      It "Artifactory password exists - $RDS"
+        When call aws_ssm_by_path "$RDS"
+        The output should include "pass"
+    End
+  End
 End
