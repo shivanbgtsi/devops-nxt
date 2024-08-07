25096ecb150eb67f288486e76e0250732806f33e
Shivanagouda Govindagoudar <68856301+shivanbgtsi@users.noreply.github.com>
2024-08-07 12:44:14 +0530

    Update packer_spec.sh

--- spec/packer_spec.sh
+++ spec/packer_spec.sh
@@ -17,14 +17,4 @@ Describe "Demo prerequisites"
         The output should include "username"
     End
   End
-  Describe "in AWS parameter store-temp"
-      It "Semarchy value exists - $RDS"
-        When call aws_ssm_by_path "$RDS"
-        The output should include "pass"
-    End
-      It "Artifactory password exists - $RDS"
-        When call aws_ssm_by_path "$RDS"
-        The output should include "pass"
-    End
-  End
 End
