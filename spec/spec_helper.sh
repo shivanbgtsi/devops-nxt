f140facf0a49570a1940cc1f2c1ba98f66318f42
Shivanagouda Govindagoudar <68856301+shivanbgtsi@users.noreply.github.com>
2024-08-07 12:47:54 +0530

    Update spec_helper.sh

--- spec/spec_helper.sh
+++ spec/spec_helper.sh
@@ -4,5 +4,5 @@ set -eu
 # Given a path, obtain the parameters under that path from parameter store.
 aws_ssm_by_path() {
   echo "ssm params"
-  aws ssm get-parameters-by-path --region eu-west-1 --output text --path "$1" --query "Parameters[*].{Name:Name,Value:Value}";
+  aws ssm get-parameters-by-path --region ap-south-1 --output text --path "$1" --query "Parameters[*].{Name:Name,Value:Value}";
 }
