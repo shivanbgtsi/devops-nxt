e74b6f82071483a7919fa1fca7f6661be4d8e96a
Shivanagouda Govindagoudar <68856301+shivanbgtsi@users.noreply.github.com>
2024-08-21 11:45:04 +0530

    Update test_spec.sh

--- spec/test_spec.sh
+++ spec/test_spec.sh
@@ -9,11 +9,11 @@ Describe "Demo prerequisites"
 
   Describe "in AWS parameter store"
       It "RDS password exists - $RDS"
-        When call aws_ssm_by_path "/dev/rds"
+        When call aws_ssm_by_path "$RDS"
         The output should include "pass"
     End
-      It "RDS user name exists - /dev/rds"
-        When call aws_ssm_by_path "/dev/rds"
+      It "RDS user name exists - $RDS"
+        When call aws_ssm_by_path "$RDS"
         The output should include "username"
     End
   End
